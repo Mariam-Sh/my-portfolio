b1abdef4c150f8d8b0e0426dfb1b227db3bfd4b5
Mariam-Sh <sharmazanyanmariam@gmail.com>
2020-07-12 23:22:21 +0400

    Updates

---
+++ precache-manifest.ee66e2799d2afeae568b56f81441b949.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec38d0770ee51705e107bc47c881eda",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "995677e47191f7d444f8",
+    "url": "/my-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9c120ac5e683bf96b596",
+    "url": "/my-portfolio/static/css/main.3563075c.chunk.css"
+  },
+  {
+    "revision": "995677e47191f7d444f8",
+    "url": "/my-portfolio/static/js/2.e0debdf2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-portfolio/static/js/2.e0debdf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c120ac5e683bf96b596",
+    "url": "/my-portfolio/static/js/main.d113e129.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "19ef1b60f9d56652158f346c3535d162",
+    "url": "/my-portfolio/static/media/footer.19ef1b60.jpg"
+  },
+  {
+    "revision": "ef13003294fb2dfa27db0ba417cb472d",
+    "url": "/my-portfolio/static/media/laptop.ef130032.jpg"
+  },
+  {
+    "revision": "ad5f611449f8efe784f2b6ed36544ab0",
+    "url": "/my-portfolio/static/media/skill.ad5f6114.jpg"
+  }
+]);
